0b3d9bee6b80676f860c7c0e1253631788d476ab
Timofey Khirianov <tkhirianov@gmail.com>
2015-10-20 09:11:10 +0300

    Результаты трудофф

---
+++ solar_input.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+# license: GPLv3
+
+from solar_objects import Star, Planet
+
+
+def read_space_objects_data_from_file(input_filename):
+    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
+    и вызывает создание их графических образов
+
+    Параметры:
+
+    **input_filename** — имя входного файла
+    """
+
+    objects = []
+    with open(input_filename) as input_file:
+        for line in input_file:
+            if len(line.strip()) == 0 or line[0] == '#':
+                continue  # пустые строки и строки-комментарии пропускаем
+            object_type = line.split()[0].lower()
+            if object_type == "star":  # FIXME: do the same for planet
+                star = Star()
+                parse_star_parameters(line, star)
+                objects.append(star)
+            else:
+                print("Unknown space object")
+
+    return objects
+
+
+def parse_star_parameters(line, star):
+    """Считывает данные о звезде из строки.
+    Входная строка должна иметь слеюущий формат:
+    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
+    Пример строки:
+    Star 10 red 1000 1 2 3 4
+
+    Параметры:
+
+    **line** — строка с описание звезды.
+    **star** — объект звезды.
+    """
+
+    pass  # FIXME: not done yet
+
+def parse_planet_parameters(line, planet):
+    """Считывает данные о планете из строки.
+    Предполагается такая строка:
+    Входная строка должна иметь слеюущий формат:
+    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
+    Пример строки:
+    Planet 10 red 1000 1 2 3 4
+
+    Параметры:
+
+    **line** — строка с описание планеты.
+    **planet** — объект планеты.
+    """
+    pass  # FIXME: not done yet...
+
+
+def write_space_objects_data_to_file(output_filename, space_objects):
+    """Сохраняет данные о космических объектах в файл.
+    Строки должны иметь следующий формат:
+    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Параметры:
+
+    **output_filename** — имя входного файла
+    **space_objects** — список объектов планет и звёзд
+    """
+    with open(output_filename, 'w') as out_file:
+        for obj in space_objects:
+            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            # FIXME: should store real values
+
+# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
---
+++ solar_model.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+# license: GPLv3
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        body.Fx += 1  # FIXME: нужно вывести формулу...
+        body.Fy += 2  # FIXME: нужно вывести формулу...
+
+
+def move_space_object(body, dt):
+    """Перемещает тело в соответствии с действующей на него силой.
+
+    Параметры:
+
+    **body** — тело, которое нужно переместить.
+    """
+
+    ax = body.Fx/body.m
+    body.x += 42  # FIXME: не понимаю как менять...
+    body.Vx += ax*dt
+    # FIXME: not done recalculation of y coordinate!
+
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **dt** — шаг по времени
+    """
+
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for body in space_objects:
+        move_space_object(body, dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")

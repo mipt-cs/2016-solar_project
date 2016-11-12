b66da983a8a9f2828b9558277a08769bba53c609
zzzuka <zuka-monstr.yes@yandex.ru>
2016-11-12 17:37:36 +0300

    fixed solar_vis and solar_input (except the writing-to-file function)

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,15 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    line = list(map(str, input().split(' ')))
+    Star.R = float(line[1])
+    Star.color = line[2]
+    Star.m = float(line[3])
+    Star.x = float(line[4])
+    Star.y = float(line[5])
+    Star.Vx = float(line[6])
+    Star.Vy  = float(line[7])
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +72,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    line = list(map(str, input().split(' ')))
+    Planet.R = float(line[1])
+    Planet.color = line[2]
+    Planet.m = float(line[3])
+    Planet.x = float(line[4])
+    Planet.y = float(line[5])
+    Planet.Vx = float(line[6])
+    Planet.Vy = float(line[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):

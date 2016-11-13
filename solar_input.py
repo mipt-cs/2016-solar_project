ccc940fa25e2815244731d433a2a4cb8887a3c22
MilitaKocharovskaya <militochka@gmail.com>
2016-11-13 19:55:23 +0300

    fixed some other mistakes

--- solar_input.py
+++ solar_input.py
@@ -95,7 +95,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy, out_file)
+            print(out_file,
+                  "%s %d %s %f %f %f %f %f" % (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 

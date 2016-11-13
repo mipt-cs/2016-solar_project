10ff9886405dc1a5a58b6bcd7f15aea0ffcab8d9
MilitaKocharovskaya <militochka@gmail.com>
2016-11-13 19:32:30 +0300

    fixed some other mistakes

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,13 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += gravitational_constant*body.m*obj.m/(body.x*obj.x)
-        body.Fy += gravitational_constant*body.m*obj.m/(body.y*obj.y)
+        dax = obj.x - body.x
+        day = obj.y - body.y
+        sin = dax / r
+        cos = day / r
+        F = gravitational_constant*body.m*obj.m/(r ** 2)
+        body.Fx += F * cos
+        body.Fy += F * sin
 
 
 def move_space_object(body, dt):
@@ -31,12 +36,12 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
-    body.x += body.x + ax*dt +ax**2*dt/2
-    body.Vx += ax*dt
+    ax = body.Fx / body.m
+    body.Vx += ax * dt
+    body.x += body.Vx * dt
     ay = body.Fy / body.m
-    body.y += body.y + ay * dt + ay ** 2 * dt / 2
     body.Vy += ay * dt
+    body.y += body.Vy * dt
 
 
 

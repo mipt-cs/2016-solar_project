5b65eabe13550f143f6452b7ac56c856f7b5724e
daedwhisperme <ekaterinacydenzhapova@mail.ru>
2016-11-15 10:52:16 +0300

    dvizenie

--- solar_model.py
+++ solar_model.py
@@ -19,11 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        try: cosa=abs(body.y-obj.y)/r
-        except: cosa=0
-        F=(G*body.m*obj.m)/r**2
-        body.Fx += F*cosa
-        body.Fy += (F**2-(F*cosa)**2)**0.5
+        body.Fx += (((obj.x-body.x)*G*obj.m*body.m)/r**2)
+        body.Fy += (((obj.y-body.y)*G*obj.m*body.m)/r**2)
 
 
 def move_space_object(body, dt):

58136758d0c2fd5e29a762e6658c153e4cfe31f1
vvernovskaya <vvernovskaya@gmail.com>
2019-11-12 15:24:57 +0300

    delete extra lines, add self.type

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":  #fixed
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -46,6 +50,7 @@ def parse_star_parameters(line, star):
 
     starparameters = line.split
     if starparameters[0] == "Star":
+        star.type = starparameters[0]
         star.R = starparameters[1]
         star.color = starparameters[2]
         star.m = starparameters[3]
@@ -53,6 +58,8 @@ def parse_star_parameters(line, star):
         star.y = starparameters[5]
         star.Vx = starparameters[6]
         star.Vy = starparameters[7]
+
+
 
 
 def parse_planet_parameters(line, planet):
@@ -72,6 +79,7 @@ def parse_planet_parameters(line, planet):
     """
     planetparameters = line.split
     if planetparameters[0] == "Planet":
+        star.type = planetparameters[0]
         planet.R = planetparameters[1]
         planet.color = planetparameters[2]
         planet.m = planetparameters[3]
@@ -94,8 +102,12 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            file_string = obj.type + ' ' + obj.color + ' ' + str(obj.m) + ' ' + str(obj.x) + ' ' + str(obj.y) + \
+                          str(obj.Vx) + ' ' + str(obj.Vy) + '\n'
+            out_file.write(file_string)
+
+
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 

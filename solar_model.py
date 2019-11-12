fa13df81382f5c3d8eff4f286254f0745742d12a
davzohr <davzohr@gmail.com>
2019-11-12 14:19:46 +0300

    fixed gravitational laws

--- solar_model.py
+++ solar_model.py
@@ -17,10 +17,11 @@ def calculate_force(body, space_objects):
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+            pass  # тело не действует гравитационной силой на само себя!
+        else:
+            r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+            body.Fx += gravitational_constant * body.m * obj.m / (r ** 3) * (obj.x - body.x)   # FIXME: нужно вывести формулу...
+            body.Fy += gravitational_constant * body.m * obj.m / (r ** 3) * (obj.y - body.y)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):

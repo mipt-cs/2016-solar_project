f9e25ab0261c8a115903bffc1dd920c4985ca0bb
IstominArseniy <istomin15arseniy@gmail.com>
2019-11-12 13:54:53 +0300

    add force calculation

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,10 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        F = gravitational_constant * body.m * obj.m / r ** 2
+        body.Fx += -F * (body.x - obj.x) / r
+        body.Fy += - (body.y - obj.y) / r
+        return [body.Fx, body.Fy]
 
 
 def move_space_object(body, dt):

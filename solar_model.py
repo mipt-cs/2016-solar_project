1f85a28cacd03e762b357519187a490b2b06774c
Darina Gaff <darinagaff@gmail.com>
2019-12-27 16:13:54 +0300

    Fixed calculate_force() and  move_space_object()

--- solar_model.py
+++ solar_model.py
@@ -14,14 +14,19 @@ def calculate_force(body, space_objects):
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
-    body.Fx = body.Fy = 0
+    body.Fx = 0
+    body.Fy = 0
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
-
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        dax = obj.x - body.x
+        day = obj.y - body.y
+        sin = day / r
+        cos = dax / r
+        F = (gravitational_constant * body.m * obj.m) / (r ** 2)
+        body.Fx += F * cos
+        body.Fy += F * sin
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
@@ -31,10 +36,12 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    ax = body.Fx / body.m
+    body.x += ax * (dt ** 2) / 2 + body.Vx * dt
+    body.Vx += ax * dt
+    ay = body.Fy / body.m
+    body.y += ay * (dt ** 2) / 2 + body.Vy * dt
+    body.Vy += ay * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):

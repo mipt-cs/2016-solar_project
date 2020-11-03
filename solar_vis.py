cd77f46db0c2100144f8c6722f10c801c6055a64
HeisenbergUncertainty <44612138+HeisenbergUncertainty@users.noreply.github.com>
2020-11-03 12:26:03 +0300

    Update solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -54,7 +54,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return -(int(x*scale_factor) + window_width//2)
+    return -(int(x*scale_factor) + window_height//2)
 
 
 def create_star_image(space, star):

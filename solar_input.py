f5ed2c1ea80406e7f08f3d6d2327ac797a591146
maryprotgit config user.name maryprotgit config user.name maryprot <protopopova.md@phystech.edu>
2020-11-06 17:47:05 +0300

    Сделана первая функция

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,16 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":  
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            else:
+                print("Unknown space object")
+            if object_type == "planet": 
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -78,6 +84,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print 
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...

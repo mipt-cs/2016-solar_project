0f8a3c16cc2fdbaeed5a3c5cc9351b65326ca612
maryprotgit config user.name maryprotgit config user.name maryprot <protopopova.md@phystech.edu>
2020-11-06 18:03:04 +0300

    Пофиксила последнюю функцию

--- solar_input.py
+++ solar_input.py
@@ -83,8 +83,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            print 
+            print(out_file, type(obj), obj.r, obj.colour, obj.m, obj.x, obj.y, obj.Vx, obj.Vy )
+    
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...

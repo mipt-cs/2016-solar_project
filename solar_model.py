7531a11722a171aa660d3576a148970e93628073
IlgamF <faliakhov.is@phystech.edu>
2020-11-09 12:08:10 +0300

    same

--- solar_model.py
+++ solar_model.py
@@ -1,12 +1,9 @@
 # coding: utf-8
 # license: GPLv3
 
-<<<<<<< HEAD
-import math as m
-=======
-import math
 
->>>>>>> 5de08bfa36a735aabbda3fe161961a32df85695b
+import math as m
+
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
@@ -25,22 +22,14 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой само на себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-<<<<<<< HEAD
+
         if body.y - obj.y == 0:
             a = m.pi / 2
         else:
             a = m.atan(body.x - obj.x / body.y - obj.y)
         body.Fx += gravitational_constant*body.m*obj.m * m.cos(a) / r**2  # FIXME: нужно вывести формулу...
         body.Fy += gravitational_constant*body.m*obj.m * m.sin(a)/ r**2 # FIXME: нужно вывести формулу...
-=======
-        if body.x == obj.x:
-            angle = math.pi/2
-        else:
-            angle = math.atan((body.y - obj.y) / (body.x - obj.x))
-        df = gravitational_constant * obj.m * body.m / r**2
-        body.Fx += df * math.cos(angle)
-        body.Fy += df * math.sin(angle)  # FIXME: нужно вывести формулу...
->>>>>>> 5de08bfa36a735aabbda3fe161961a32df85695b
+
 
 
 def move_space_object(body, dt):
@@ -51,11 +40,7 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить
     """
 
-<<<<<<< HEAD
     ax = -body.Fx / body.m
-=======
-    ax = body.Fx
->>>>>>> 5de08bfa36a735aabbda3fe161961a32df85695b
     body.Vx += ax*dt
     body.x += body.Vx * dt
 

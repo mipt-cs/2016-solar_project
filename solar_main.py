f91fdccb518921e746df4cec42477a803b6a6d32
Ivan <allvisionk@mail.ru>
2020-11-11 08:46:16 +0300

    added write stats button

--- solar_main.py
+++ solar_main.py
@@ -83,6 +83,7 @@ def open_file_dialog():
     space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
+    delete_last_stats('stats.txt')
 
     for obj in space_objects:
         if obj.type == 'star':

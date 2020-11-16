cb842683b3367329bacd61d09e783cfd474b9742
KleimenovM <kleimenov.max@phystech.edu>
2020-11-16 10:59:23 +0300

    Создал дополнительный файл-библиотеку 'solar_graphics' для рисования графиков. Там пока ничего нет (совсем)

---
+++ solar_graphics.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# license: GPLv3
+
+"""
+Модуль построения графиков различных зависимостей
+"""
+
+from matplotlib import pyplot as pl
+
+
+def get_moment(obj, time):  # собирает информацию об объекте в некоторый момент времени
+
+    pass
+
+
+def draw_graph():  # строит необходимый график
+
+    pass
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")

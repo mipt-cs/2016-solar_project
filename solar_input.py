1e89898038072db39db0e05a25234924b79c4b9d
ArtemEvstafev <vur1artem1cheb@gmail.com>
2021-03-29 19:39:05 +0300

    calculate_force and move_space_object done

--- solar_input.py
+++ solar_input.py
@@ -93,7 +93,6 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
-
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 if __name__ == "__main__":

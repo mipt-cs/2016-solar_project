0d0e7baee9ab3ca6f3cb959fdde8c4a3b1db8ee4
Diana2410-start <yakusheva2410@yandex.ru>
2021-03-29 18:37:15 +0300

    solar

--- solar_input.py
+++ solar_input.py
@@ -2,6 +2,7 @@
 # license: GPLv3
 
 from solar_objects import Star, Planet
+from tkinter import messagebox
 
 
 def read_space_objects_data_from_file(input_filename):
@@ -26,7 +27,19 @@ def read_space_objects_data_from_file(input_filename):
             else:
                 print("Unknown space object")
 
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
+            else:
+                print("Unknown space object")
+
     return objects
+
+
+def display_full_name():
+    messagebox.showinfo(" Python",
+                        radius.get() + " " + colour.get() + " " + weight.get() + " " + coord_x.get() + " " + coord_y.get() + " " + coord_vx.get() + " " + coord_vy.get())
 
 
 def parse_star_parameters(line, star):
@@ -43,8 +56,54 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    root = Tk()
+    root.title(" Star")
+
+    radius = StringVar()
+    colour = StringVar()
+    weight = StringVar()
+    coord_x = StringVar()
+    coord_y = StringVar()
+    coord_vx = StringVar()
+    coord_vy = StringVar()
+
+    radius_label = Label(text="Введите радиус в пикселях:")
+    colour_label = Label(text="Введите цвет:")
+    weight_label = Label(text="Введите массу:")
+    coord_x_label = Label(text="Введите координату по x:")
+    coord_y_label = Label(text="Введите координату по y:")
+    coord_vx_label = Label(text="Введите скорость по x:")
+    coord_vy_label = Label(text="Введите скорость по y:")
+
+    radius_label.grid(row=0, column=0, sticky="w")
+    colour_label.grid(row=1, column=0, sticky="w")
+    weight_label.grid(row=1, column=0, sticky="w")
+    coord_x_label.grid(row=1, column=0, sticky="w")
+    coord_y_label.grid(row=1, column=0, sticky="w")
+    coord_vx_label.grid(row=1, column=0, sticky="w")
+    coord_vy_label.grid(row=1, column=0, sticky="w")
+
+    radius_entry = Entry(textvariable=radius)
+    colour_entry = Entry(textvariable=colour)
+    weight_entry = Entry(textvariable=weight)
+    coord_x_entry = Entry(textvariable=coord_x)
+    coord_y_entry = Entry(textvariable=coord_y)
+    coord_vx_entry = Entry(textvariable=coord_vx)
+    coord_vy_entry = Entry(textvariable=coord_vy)
+
+    radius_entry.grid(row=0, column=1, padx=5, pady=5)
+    colour_entry.grid(row=1, column=1, padx=5, pady=5)
+    weight_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_x_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_y_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_vx_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_vy_entry.grid(row=1, column=1, padx=5, pady=5)
+
+    message_button = Button(text="Click Me", command=display_full_name)
+    message_button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
     pass  # FIXME: not done yet
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,6 +120,51 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
+    root = Tk()
+    root.title(" Planet ")
+
+    radius = StringVar()
+    colour = StringVar()
+    weight = StringVar()
+    coord_x = StringVar()
+    coord_y = StringVar()
+    coord_vx = StringVar()
+    coord_vy = StringVar()
+
+    radius_label = Label(text="Введите радиус в пикселях:")
+    colour_label = Label(text="Введите цвет:")
+    weight_label = Label(text="Введите массу:")
+    coord_x_label = Label(text="Введите координату по x:")
+    coord_y_label = Label(text="Введите координату по y:")
+    coord_vx_label = Label(text="Введите скорость по x:")
+    coord_vy_label = Label(text="Введите скорость по y:")
+
+    radius_label.grid(row=0, column=0, sticky="w")
+    colour_label.grid(row=1, column=0, sticky="w")
+    weight_label.grid(row=1, column=0, sticky="w")
+    coord_x_label.grid(row=1, column=0, sticky="w")
+    coord_y_label.grid(row=1, column=0, sticky="w")
+    coord_vx_label.grid(row=1, column=0, sticky="w")
+    coord_vy_label.grid(row=1, column=0, sticky="w")
+
+    radius_entry = Entry(textvariable=radius)
+    colour_entry = Entry(textvariable=colour)
+    weight_entry = Entry(textvariable=weight)
+    coord_x_entry = Entry(textvariable=coord_x)
+    coord_y_entry = Entry(textvariable=coord_y)
+    coord_vx_entry = Entry(textvariable=coord_vx)
+    coord_vy_entry = Entry(textvariable=coord_vy)
+
+    radius_entry.grid(row=0, column=1, padx=5, pady=5)
+    colour_entry.grid(row=1, column=1, padx=5, pady=5)
+    weight_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_x_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_y_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_vx_entry.grid(row=1, column=1, padx=5, pady=5)
+    coord_vy_entry.grid(row=1, column=1, padx=5, pady=5)
+
+    message_button = Button(text="Click Me", command=display_full_name)
+    message_button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
     pass  # FIXME: not done yet...
 
 
@@ -79,6 +183,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 

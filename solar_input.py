5baa5060d3bb5b55ae5809d494f28a2245e7ee0c
Bionhen <liubimov.is@phystech.edu>
2021-11-09 14:01:18 +0300

    Fixed parsing

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,18 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star_list = []
+    for i in range(len(line)): 
+        if line[i] == ' ': 
+            starlist.append(i)
+    star.r = int(line[star_list[0]+1:star_list[1]-1])
+    star.color = (line[star_list[1] + 1:star_list[2] - 1])
+    star.m = int(line[star_list[2] + 1:star_list[3] - 1])
+    star.x = int(line[star_list[3] + 1:star_list[4] - 1])
+    star.y = int(line[star_list[4] + 1:star_list[5] - 1])
+    star.Vx = int(line[star_list[5] + 1:star_list[6] - 1])
+    star.Vy = int(line[star_list[6] + 1:star_list[7] - 1])
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +75,17 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet_list = []
+    for i in range(len(line)):
+        if line[i] == ' ':
+            planetlist.append(i)
+    planet.r = int(line[planet_list[0] + 1:planet_list[1] - 1])
+    planet.color = (line[planet_list[1] + 1:planet_list[2] - 1])
+    planet.m = int(line[planet_list[2] + 1:planet_list[3] - 1])
+    planet.x = int(line[planet_list[3] + 1:planet_list[4] - 1])
+    planet.y = int(line[planet_list[4] + 1:planet_list[5] - 1])
+    planet.Vx = int(line[planet_list[5] + 1:planet_list[6] - 1])
+    planet.Vy = int(line[planet_list[6] + 1:planet_list[7] - 1])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -81,6 +105,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")

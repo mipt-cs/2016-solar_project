4da693eded3beeced2ea587a128648930c283b59
Matvey <samsonov.ma@phystech.edu>
2021-11-10 02:09:52 +0300

    Fixed call-blocking issues with all functions in solar_input. They are now all working as intended

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star": 
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":  
+                planet = Planet()
+                parse_planet_parameters(line, star)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,7 +48,11 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    for attr, val in zip('R color m x y Vx Vy'.split(), 
+        line.strip().split()[1:]):
+        setattr(star, attr, val if attr=='color' else float(val))
+
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +69,9 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    for attr, val in zip('R color m x y Vx Vy'.split(), 
+        line.strip().split()[1:]):
+        setattr(planet, attr, val if attr=='color' else float(val))
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -75,10 +85,13 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
-    with open(output_filename, 'w') as out_file:
+    with open(output_filename, 'a') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            attribute_string = obj.type + ' '
+            for attr in 'R color m x y Vx Vy'.split():
+                attribute_string += f"{getattr(obj, attr)} "
+            print(attribute_string.strip(), file=out_file)
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 

f3c11397da4f69a280fd39d4f3c42002537efe84
zhigalskiiivan <zhigalskii.ia@phystech.edu>
2021-11-11 02:59:47 +0300

    упрощены функции считывания данных из файла, создана функция записи данных в файл

--- solar_input.py
+++ solar_input.py
@@ -5,7 +5,7 @@ from solar_objects import Star, Planet
 
 
 def read_space_objects_data_from_file(input_filename):
-    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
+    """ Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
 
     Параметры:
@@ -21,11 +21,11 @@ def read_space_objects_data_from_file(input_filename):
             object_type = line.split()[0].lower()
             if object_type == "star":
                 star = Star()
-                parse_star_parameters(line, star)
+                star = parse_star_parameters(line, star)
                 objects.append(star)
             elif object_type == "planet":
                 planet = Planet()
-                parse_planet_parameters(line, planet)
+                planet = parse_planet_parameters(line, planet)
                 objects.append(planet)
             else:
                 print("Unknown space object")
@@ -34,7 +34,7 @@ def read_space_objects_data_from_file(input_filename):
 
 
 def parse_star_parameters(line, star):
-    """Считывает данные о звезде из строки.
+    """ Считывает данные о звезде из строки.
     Входная строка должна иметь слеюущий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
@@ -44,26 +44,25 @@ def parse_star_parameters(line, star):
 
     Параметры:
 
-    **line** — строка с описание звезды.
+    **line** — строка с описанием звезды.
     **star** — объект звезды.
 
-    Нумерует пробелы. Радиус - то, что между первым и вторым пробелом в строке, и так далее.
     """
-    star_list = []
-    for i in range(len(line)):
-        if line[i] == ' ':
-            starlist.append(i)
-    star.r = int(line[star_list[0] + 1:star_list[1]])
-    star.color = (line[star_list[1] + 1:star_list[2]])
-    star.m = int(line[star_list[2] + 1:star_list[3]])
-    star.x = int(line[star_list[3] + 1:star_list[4]])
-    star.y = int(line[star_list[4] + 1:star_list[5]])
-    star.Vx = int(line[star_list[5] + 1:star_list[6]])
-    star.Vy = int(line[star_list[6] + 1:])
+
+    obj_type, r, color, m, x, y, vx, vy = line.split()
+    star.R = float(r)
+    star.color = color
+    star.m = float(m)
+    star.x = float(x)
+    star.y = float(y)
+    star.Vx = float(vx)
+    star.Vy = float(vy)
+
+    return star
 
 
 def parse_planet_parameters(line, planet):
-    """Считывает данные о планете из строки.
+    """ Считывает данные о планете из строки.
     Предполагается такая строка:
     Входная строка должна иметь слеюущий формат:
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
@@ -79,17 +78,17 @@ def parse_planet_parameters(line, planet):
 
     Нумерует пробелы. Радиус - то, что между первым и вторым пробелом в строке, и так далее.
     """
-    planet_list = []
-    for i in range(len(line)):
-        if line[i] == ' ':
-            planet_list.append(i)
-    planet.r = int(line[planet_list[0] + 1:planet_list[1]])
-    planet.color = (line[planet_list[1] + 1:planet_list[2]])
-    planet.m = int(line[planet_list[2] + 1:planet_list[3]])
-    planet.x = int(line[planet_list[3] + 1:planet_list[4]])
-    planet.y = int(line[planet_list[4] + 1:planet_list[5]])
-    planet.Vx = int(line[planet_list[5] + 1:planet_list[6]])
-    planet.Vy = int(line[planet_list[6] + 1:])
+
+    obj_type, r, color, m, x, y, vx, vy = line.split()
+    planet.R = float(r)
+    planet.color = color
+    planet.m = float(m)
+    planet.x = float(x)
+    planet.y = float(y)
+    planet.Vx = float(vx)
+    planet.Vy = float(vy)
+
+    return planet
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -105,10 +104,12 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            obj_type, r, color, m, x, y, vx, vy = map(str,
+                                                      [obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx,
+                                                       obj.Vy])
+            out_file.write(
+                obj_type + " " + r + " " + color + " " + m + " " + x + " " + y + " " + vx + " " + vy + "\n")
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 if __name__ == "__main__":
     print("This module is not for direct call!")

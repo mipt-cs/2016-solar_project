284c7ff4ab6164b7e255c89e0422879267211828
Zigismund <nikchersib@gmail.com>
2021-11-14 05:15:30 +0700

    Some changes in visual module by A programmer

--- solar_vis.py
+++ solar_vis.py
@@ -9,10 +9,10 @@
 header_font = "Arial-16"
 """Шрифт в заголовке"""
 
-window_width = 700
+window_width = 800
 """Ширина окна"""
 
-window_height = 700
+window_height = 800
 """Высота окна"""
 
 scale_factor = None

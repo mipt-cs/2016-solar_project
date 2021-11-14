f87f9fe6df722bc4699b7603a6321d00b7a7187b
Bionhen <liubimov.is@phystech.edu>
2021-11-14 21:28:20 +0300

    motion

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += G*obj.m*body.m*(obj.x - body.x)/r**(3/2)
-        body.Fy += G*obj.m*body.m*(obj.y - body.y)/r**(3/2)
+        body.Fx += G*obj.m*body.m*(obj.x - body.x)/(r**3)
+        body.Fy += G*obj.m*body.m*(obj.y - body.y)/(r**3)
 
 
 def move_space_object(body, dt):

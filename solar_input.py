b0f61070d15804b6ad0d9675e894b155db5ed25b
FalconChrome <tastylifeeel03@mail.ru>
2021-11-20 03:07:25 +0300

    Fixed writing data to file

--- solar_input.py
+++ solar_input.py
@@ -118,8 +118,10 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            data = [obj.type, obj.R, obj.color, obj.m,
+                    obj.x, obj.y, obj.Vx, obj.Vy]
+            print(' '.join(map(str, data)), file=out_file)
+            # FIXED: store real values of parameters in file
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл
 

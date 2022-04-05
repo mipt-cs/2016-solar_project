47b0feddf753e0cf228d6afba89d54df05533e05
Rogodata <shumov.ai@phystech.edu>
2022-04-05 16:33:42 +0300

    Changed file_redaing functions

--- solar_input.py
+++ solar_input.py
@@ -23,9 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
-            else:
-                print("Unknown space object")
-
+            elif object_type == "planet":
+                planet = Planet()
+            parse_planet_parameters(line, planet)
+            objects.append(planet)
     return objects
 
 
@@ -43,8 +44,10 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    (star.R, star.color, star.m, star.x, star.y, star.Vx, star.Vy) = (
+        int(line[1]), line[2], int(line[3]), int(line[4]), int(line[5]), int(line[6]), int(line[7]))
+    # FIXME: not done yet. Change names? they are global now
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,6 +64,8 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
+    (planet.R, planet.color, planet.m, planet.x, planet.y, planet.Vx, planet.Vy) = (
+        int(line[1]), line[2], int(line[3]), int(line[4]), int(line[5]), int(line[6]), int(line[7]))
     pass  # FIXME: not done yet...
 
 
@@ -79,6 +84,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 

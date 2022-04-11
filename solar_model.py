5da5385896aea55fe033f3ebc2ffeeefd96b5eec
Ivan Khripunov <khripunov.iv@phystech.edu>
2022-04-11 23:30:22 +0300

    solar_model fixed

--- solar_model.py
+++ solar_model.py
@@ -20,7 +20,7 @@ def calculate_force(body, space_objects):
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
         body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (- (body.x - obj.x))
-        body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (-(body.y - obj.y))
+        body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (- (body.y - obj.y))
 
 
 def move_space_object(body, dt):
@@ -32,14 +32,14 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx / body.m
-    body.x += body.Vx * dt
     body.Vx += ax * dt
+    body.x += body.Vx * dt
 
     ay = body.Fy / body.m
-    body.y += body.Vy * dt
     body.Vy += ay * dt
-    print(body.color, body.x, body.y)
+    body.y += body.Vy * dt
 
+    print(body.color, body.x, body.y)
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.

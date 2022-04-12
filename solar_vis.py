ef1de86e248bcf89e40f184fea940784be752c07
Ivan Khripunov <khripunov.iv@phystech.edu>
2022-04-12 11:04:54 +0300

    solar_vis refactoring done

--- solar_vis.py
+++ solar_vis.py
@@ -6,112 +6,121 @@
 Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
 """
 
-header_font = "Arial-16"
+HEADER_FONT = "Arial-16"
 """Шрифт в заголовке"""
 
-window_width = 800
+WINDOW_WIDTH = 800
 """Ширина окна"""
 
-window_height = 800
+WINDOW_HEIGHT = 800
 """Высота окна"""
 
-scale_factor = None
+
+class ScaleFactor:
+    """
+    Показатель масштабирования
+    """
+    value = None
+
+
 """Масштабирование экранных координат по отношению к физическим.
 Тип: float
 Мера: количество пикселей на один метр."""
 
 
-def calculate_scale_factor(max_distance):
-    """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
-    global scale_factor
-    scale_factor = 0.4 * min(window_height, window_width) / max_distance
-    print('Scale factor:', scale_factor)
+def calculate_scale_factor(max_distance, scale_factor_class):
+    """
+    Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине
+    :param max_distance: физирческий размер поля
+    :param scale_factor_class: класс, хранящий показатель масштабирования
+    """
+    scale_factor_class.value = 0.4 * min(WINDOW_HEIGHT, WINDOW_WIDTH) / max_distance
+    print('Scale factor:', scale_factor_class.value)
 
 
-def scale_x(x):
-    """Возвращает экранную **x** координату по **x** координате модели.
+def scale_x(x, scale_factor_class):
+    """
+    Возвращает экранную **x** координату по **x** координате модели.
     Принимает вещественное число, возвращает целое число.
     В случае выхода **x** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
 
-    Параметры:
-
-    **x** — x-координата модели.
+    :param x: координата
+    :param scale_factor_class: класс, хранящий показатель масштабирования
     """
 
-    return int(x * scale_factor) + window_width // 2
+    return int(x * scale_factor_class.value) + WINDOW_WIDTH // 2
 
 
-def scale_y(y):
-    """Возвращает экранную **y** координату по **y** координате модели.
+def scale_y(y, scale_factor_class):
+    """
+    Возвращает экранную **y** координату по **y** координате модели.
     Принимает вещественное число, возвращает целое число.
     В случае выхода **y** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
     Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
 
-    Параметры:
-
-    **y** — y-координата модели.
+    :param y: y-координата модели
+    :param scale_factor_class: класс, хранящий показатель масштабирования
     """
 
-    return - int(y * scale_factor) + window_height // 2
+    return - int(y * scale_factor_class.value) + WINDOW_HEIGHT // 2
 
 
-def create_star_image(space, star):
-    """Создаёт отображаемый объект звезды.
+def create_star_image(space, star, scale_factor_class):
+    """
+    Создание отображаемого объекта звезды.
 
-    Параметры:
-
-    **space** — холст для рисования.
-    **star** — объект звезды.
+    :param space: холст для рисования
+    :param star: звезда
+    :param scale_factor_class: класс, хранящий показатель масштабирования
     """
 
-    x = scale_x(star.x)
-    y = scale_y(star.y)
+    x = scale_x(star.x, scale_factor_class)
+    y = scale_y(star.y, scale_factor_class)
     r = star.R
     star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
 
 
-def create_planet_image(space, planet):
-    """Создаёт отображаемый объект планеты.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **planet** — объект планеты.
+def create_planet_image(space, planet, scale_factor_class):
     """
-    x = scale_x(planet.x)
-    y = scale_y(planet.y)
+    Создаёт отображаемый объект планеты.
+
+    :param space: холст для рисования.
+    :param planet: объект планеты.
+    :param scale_factor_class: класс, хранящий показатель масштабирования
+    """
+    x = scale_x(planet.x, scale_factor_class)
+    y = scale_y(planet.y, scale_factor_class)
     r = planet.R
     planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
-    """Создаёт на холсте текст с названием системы небесных тел.
+    """
+    Создаёт на холсте текст с названием системы небесных тел.
     Если текст уже был, обновляет его содержание.
 
-    Параметры:
-
-    **space** — холст для рисования.
-    **system_name** — название системы тел.
+    :param space: холст для рисования.
+    :param system_name: — название системы тел.
     """
-    space.create_text(30, 80, tag="header", text=system_name, font=header_font)
+    space.create_text(30, 80, tag="header", text=system_name, font=HEADER_FONT)
 
 
-def update_object_position(space, body):
-    """Перемещает отображаемый объект на холсте.
-
-    Параметры:
-
-    **space** — холст для рисования.
-    **body** — тело, которое нужно переместить.
+def update_object_position(space, body, scale_factor_class):
     """
-    x = scale_x(body.x)
-    y = scale_y(body.y)
+    Перемещает отображаемый объект на холсте.
+
+    :param space: — холст для рисования.
+    :param body: — тело, которое нужно переместить.
+    :param scale_factor_class: класс, хранящий показатель масштабирования
+    """
+    x = scale_x(body.x, scale_factor_class)
+    y = scale_y(body.y, scale_factor_class)
     r = body.R
-    if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
-        space.coords(body.image, window_width + r, window_height + r,
-                     window_width + 2 * r, window_height + 2 * r)  # положить за пределы окна
+    if x + r < 0 or x - r > WINDOW_WIDTH or y + r < 0 or y - r > WINDOW_HEIGHT:
+        space.coords(body.image, WINDOW_WIDTH + r, WINDOW_HEIGHT + r,
+                     WINDOW_WIDTH + 2 * r, WINDOW_HEIGHT + 2 * r)
     space.coords(body.image, x - r, y - r, x + r, y + r)
 
     print("drawing")

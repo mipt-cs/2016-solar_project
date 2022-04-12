8716d5c3cb217e16aa14aadf11d3d9a65123d10b
Kutorkin Nikolay <kutorkin.ne@phystech.edu>
2022-04-12 13:25:09 +0300

    Убрал пару глобалов

--- solar_main.py
+++ solar_main.py
@@ -7,7 +7,7 @@ from solar_vis import *
 from solar_model import *
 from solar_input import *
 
-perform_execution = False
+perform_execution = [False]
 """Флаг цикличности выполнения расчёта"""
 
 physical_time = 0
@@ -26,57 +26,54 @@ space_objects = []
 """Список космических объектов."""
 
 
-def execution():
+def execution(perform, physical_time):
     """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
     а также обновляя их положение на экране.
     Цикличность выполнения зависит от значения глобальной переменной perform_execution.
     При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
     """
-    global physical_time
+    #global physical_time
     global displayed_time
     recalculate_space_objects_positions(space_objects, time_step.get())
     for body in space_objects:
-        print(body.x, body.y)
+        # print(body.x, body.y) удалить в конце
         update_object_position(space, body)
-    physical_time += time_step.get()
-    displayed_time.set("%.1f" % physical_time + " seconds gone")
+    physical_time[0] += time_step.get()
+    displayed_time.set("%.1f" % physical_time[0] + " seconds gone")
+    if perform[0]:
+        space.after(101 - int(time_speed.get()), lambda: execution(perform, physical_time))
 
-    if perform_execution:
-        space.after(101 - int(time_speed.get()), execution)
 
-
-def start_execution():
+def start_execution(perform, button, physical_time):
     """Обработчик события нажатия на кнопку Start.
     Запускает циклическое исполнение функции execution.
     """
-    global perform_execution
-    perform_execution = True
-    start_button['text'] = "Pause"
-    start_button['command'] = stop_execution
 
-    execution()
+    perform[0] = True
+    button['text'] = "Pause"
+    button['command'] = lambda: stop_execution(perform, button, physical_time)
+
+    execution(perform, physical_time)
     print('Started execution...')
 
 
-def stop_execution():
+def stop_execution(perform, button, physical_time):
     """Обработчик события нажатия на кнопку Start.
     Останавливает циклическое исполнение функции execution.
     """
-    global perform_execution
-    perform_execution = False
-    start_button['text'] = "Start"
-    start_button['command'] = start_execution
+    perform[0] = False
+    button['text'] = "Start"
+    button['command'] = lambda: start_execution(perform, button, physical_time)
     print('Paused execution.')
 
 
-def open_file_dialog():
+def open_file_dialog(perform):
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
     """
     global space_objects
-    global perform_execution
-    perform_execution = False
+    perform[0] = False
     for obj in space_objects:
         space.delete(obj.image)  # удаление старых изображений планет
     in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
@@ -106,15 +103,15 @@ def main():
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-    global physical_time
+    #global physical_time
     global displayed_time
     global time_step
     global time_speed
     global space
-    global start_button
+    #global start_button
 
     print('Modelling started!')
-    physical_time = 0
+    physical_time = [0]
 
     root = tkinter.Tk()
     # космическое пространство отображается на холсте типа Canvas
@@ -124,7 +121,7 @@ def main():
     frame = tkinter.Frame(root)
     frame.pack(side=tkinter.BOTTOM)
 
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
+    start_button = tkinter.Button(frame, text="Start", command=lambda: start_execution(perform_execution, start_button, physical_time), width=6)
     start_button.pack(side=tkinter.LEFT)
 
     time_step = tkinter.DoubleVar()
@@ -136,7 +133,7 @@ def main():
     scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
     scale.pack(side=tkinter.LEFT)
 
-    load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
+    load_file_button = tkinter.Button(frame, text="Open file...", command=lambda: open_file_dialog(perform_execution))
     load_file_button.pack(side=tkinter.LEFT)
     save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
     save_file_button.pack(side=tkinter.LEFT)

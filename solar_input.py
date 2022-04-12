69e521d0f0c34a0c55e14cb9b42ab719cc4657cd
Rogodata <shumov.ai@phystech.edu>
2022-04-12 13:04:58 +0300

    added parameters

--- solar_input.py
+++ solar_input.py
@@ -52,12 +52,14 @@ def read_space_objects_data_from_file(input_filename):
 
 
 def parse_object_parameters(parameters_line, target_obj):
-    """Считывает данные о космическом объекте из строки.
+    """Считывает данные о космическом объекте из строки и записывает их в поля объекта класса SpaceObject.
     Входная строка должна иметь слеюущий формат:
     <тип объекта> <радиус в пикселях> <цвет> <масса> <x> <y> <Vx> <Vy>
     Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
     Пример строки:
     Planet 10 red 1000 1 2 3 4
+    :param: parameters_line строка в котрой черезе пробел записаны параметры объекта
+    :param: target_obj объект класса SpaceObject, в поля котрого будем записывать данные
     """
     line_quantities = parameters_line.split()
     (target_obj.type, target_obj.R, target_obj.color, target_obj.m, target_obj.x, target_obj.y, target_obj.Vx,

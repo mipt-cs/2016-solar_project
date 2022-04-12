efaa7a60155caf256fb1f15c43f97d8dba1b157e
Rogodata <shumov.ai@phystech.edu>
2022-04-12 11:50:28 +0300

    added file_saving

--- solar_input.py
+++ solar_input.py
@@ -12,14 +12,18 @@ def read_float_quantity(number_str):
         stepen = ""
         stepen_flag = 0
         for i in range(len(number_str)):
-            if not number_str[i].isalpha():
+            if not number_str[i] == "e" or number_str[i] == "E":
                 if stepen_flag:
                     stepen += number_str[i]
                 else:
                     chislo += number_str[i]
-            else: stepen_flag = 1
+            else:
+                stepen_flag = 1
         print(stepen, chislo)
-        return float(chislo) * (10 ** int(stepen))
+        if stepen != "":
+            return float(chislo) * (10 ** int(stepen))
+        else:
+            return float(chislo)
 
 
 def read_space_objects_data_from_file(input_filename):
@@ -83,8 +87,7 @@ def parse_planet_parameters(parameters_line, planet_obj):
         read_float_quantity(line_quantities[7]))
 
 
-
-def write_space_objects_data_to_file(output_filename, space_objects):
+def write_space_objects_data_to_file(output_filename, objects_array):
     """Сохраняет данные о космических объектах в файл.
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
@@ -95,13 +98,15 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
+    write_lines = []
+    for obj in objects_array:
+        write_lines += str(obj.type) + " " + str(obj.R) + " " + str(obj.color) + " " + str(obj.m) + " " + str(
+            obj.x) + " " + str(obj.y) + " " + str(obj.Vx) + " " + str(obj.Vy) + "\n"
     with open(output_filename, 'w') as out_file:
-        for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+        out_file.writelines(write_lines)
 
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл... через writelines
 
 if __name__ == "__main__":
     print("This module is not for direct call!")

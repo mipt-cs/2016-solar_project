7a7e951f26cbdfc8751147ab7997be127ec9c502
Ivan Khripunov <khripunov.iv@phystech.edu>
2022-04-12 11:17:53 +0300

    solar_model refactoring done

--- solar_model.py
+++ solar_model.py
@@ -1,17 +1,16 @@
 # coding: utf-8
 # license: GPLv3
 
-gravitational_constant = 6.67408E-11
+GRAVITATIONAL_CONSTANT = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
 
 def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
+    """
+    Вычисляет силу, действующую на тело.
 
-    Параметры:
-
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
+    :param body: тело, для которого нужно вычислить действующую силу.
+    :param space_objects: список объектов, которые воздействуют на тело.
     """
 
     body.Fx = body.Fy = 0
@@ -19,16 +18,16 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
-        body.Fx += gravitational_constant * body.m * obj.m / r ** 3 * (- (body.x - obj.x))
-        body.Fy += gravitational_constant * body.m * obj.m / r ** 3 * (- (body.y - obj.y))
+        body.Fx += GRAVITATIONAL_CONSTANT * body.m * obj.m / r ** 3 * (- (body.x - obj.x))
+        body.Fy += GRAVITATIONAL_CONSTANT * body.m * obj.m / r ** 3 * (- (body.y - obj.y))
 
 
 def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
+    """
+    Перемещает тело в соответствии с действующей на него силой.
 
-    Параметры:
-
-    **body** — тело, которое нужно переместить.
+    :param body: тело, которое нужно переместить.
+    :param dt: шаг по времени
     """
 
     ax = body.Fx / body.m
@@ -41,19 +40,19 @@ def move_space_object(body, dt):
 
     print(body.color, body.x, body.y)
 
+
 def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
+    """
+    Пересчитывает координаты объектов.
 
-    Параметры:
-
-    **space_objects** — список объектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
+    :param space_objects: список объектов, для которых нужно пересчитать координаты.
+    :param dt: шаг по времени
     """
 
     for body in space_objects:
         calculate_force(body, space_objects)
         move_space_object(body, dt)
-    print("reacalculating")
+    print("recalculating")
     return space_objects
 
 

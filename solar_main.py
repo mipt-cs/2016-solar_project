09a08bc7ae1689cfd19b928f2a035e75d0ba6b6c
Ivan Khripunov <khripunov.iv@phystech.edu>
2022-04-12 10:12:56 +0300

    solar_main refactoring in progress

--- solar_main.py
+++ solar_main.py
@@ -1,34 +1,11 @@
 # coding: utf-8
 # license: GPLv3
 
-# FIXME Убрать import*
-import time
 import tkinter
-from tkinter.filedialog import *
-from solar_vis import *
-from solar_model import *
-from solar_input import *
-
-# Hello
-'''
-perform_execution = False
-"""Флаг цикличности выполнения расчёта"""
-
-physical_time = 0
-"""Физическое время от начала расчёта.
-Тип: float"""
-
-displayed_time = None
-"""Отображаемое на экране время.
-Тип: переменная tkinter"""
-
-time_step = None
-"""Шаг по времени при моделировании.
-Тип: float"""
-
-space_objects = []
-"""Список космических объектов."""
-'''
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import solar_vis
+import solar_model
+import solar_input
 
 
 class Quantities:
@@ -46,29 +23,33 @@ class Quantities:
 
 
 def execution(quantities_class):
-    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
+    """
+    Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
     а также обновляя их положение на экране.
     Цикличность выполнения зависит от значения глобальной переменной perform_execution.
     При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
-    quantities_class.space_objects = recalculate_space_objects_positions(quantities_class.space_objects,
-                                                                         quantities_class.time_step.get())
+    quantities_class.space_objects = solar_model.recalculate_space_objects_positions(quantities_class.space_objects,
+                                                                                     quantities_class.time_step.get())
     for body in quantities_class.space_objects:
-        update_object_position(quantities_class.space, body)
+        solar_vis.update_object_position(quantities_class.space, body)
     quantities_class.physical_time += quantities_class.time_step.get()
     quantities_class.displayed_time.set("%.1f" % quantities_class.physical_time + " seconds gone")
 
     print("execute")
 
     if quantities_class.perform_execution:
-        quantities_class.space.after(101 - int(quantities_class.time_speed.get()), lambda :  execution(quantities_class))
+        quantities_class.space.after(101 - int(quantities_class.time_speed.get()), lambda: execution(quantities_class))
 
 
 def start_execution(quantities_class):
     """Обработчик события нажатия на кнопку Start.
     Запускает циклическое исполнение функции execution.
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
-    # global perform_execution
     quantities_class.perform_execution = True
     quantities_class.start_button['text'] = "Pause"
     quantities_class.start_button['command'] = lambda: stop_execution(quantities_class)
@@ -77,11 +58,11 @@ def start_execution(quantities_class):
     print('Started execution...')
 
 
-# FIXME Эта штука пытается вернуть какое-то значение, а не изменить объект класса
-
 def stop_execution(quantities_class):
     """Обработчик события нажатия на кнопку Start.
     Останавливает циклическое исполнение функции execution.
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
 
     quantities_class.perform_execution = False
@@ -94,22 +75,23 @@ def open_file_dialog(quantities_class):
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
-    # global space_objects
-    # global perform_execution
+
     quantities_class.perform_execution = False
     for obj in quantities_class.space_objects:
         quantities_class.space.delete(obj.image)  # удаление старых изображений планет
     in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
-    quantities_class.space_objects = read_space_objects_data_from_file(in_filename)
+    quantities_class.space_objects = solar_input.read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in quantities_class.space_objects])
-    calculate_scale_factor(max_distance)
+    solar_vis.calculate_scale_factor(max_distance)
 
     for obj in quantities_class.space_objects:
         if obj.type == 'star':
-            create_star_image(quantities_class.space, obj)
+            solar_vis.create_star_image(quantities_class.space, obj)
         elif obj.type == 'planet':
-            create_planet_image(quantities_class.space, obj)
+            solar_vis.create_planet_image(quantities_class.space, obj)
         else:
             raise AssertionError()
 
@@ -118,30 +100,27 @@ def save_file_dialog(quantities_class):
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
     out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
-    write_space_objects_data_to_file(out_filename, quantities_class.space_objects)
+    solar_input.write_space_objects_data_to_file(out_filename, quantities_class.space_objects)
 
 
 def main(quantities_class):
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
+
+    :param quantities_class: класс, хранящий глобальные переменные
     """
-    physical_time = quantities_class.physical_time
-    displayed_time = quantities_class.displayed_time
-    time_step = quantities_class.time_step
-    time_speed = quantities_class.time_speed
-    space = quantities_class.space
-    start_button = quantities_class.start_button
 
     print('Modelling started!')
     quantities_class.physical_time = 0
 
     root = tkinter.Tk()
-    # космическое пространство отображается на холсте типа Canvas
-    quantities_class.space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
+    quantities_class.space = tkinter.Canvas(root, width=solar_vis.window_width, height=solar_vis.window_height,
+                                            bg="black")
     quantities_class.space.pack(side=tkinter.TOP)
-    # нижняя панель с кнопками
     frame = tkinter.Frame(root)
     frame.pack(side=tkinter.BOTTOM)
 
@@ -172,7 +151,6 @@ def main(quantities_class):
     print('Modelling finished!')
 
 
-quantities = Quantities()
-
 if __name__ == "__main__":
+    quantities = Quantities()
     main(quantities)
